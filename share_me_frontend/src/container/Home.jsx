98f817002a45fcc3ad8b9e99bf024805ebaf855a
aminos02 <azzouzamine02@gmail.com>
2022-01-05 14:05:56 +0100

    frontV1

---
+++ share_me_frontend/src/container/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="flex justify-start items-start flex-col h-screen">
+        Hello world!
+      </div>
+    )
+}
+
+export default Home
